36c767e8012e34e5bc34b67128dc8b27d6aa6738
Davyd Shtopprl <davyd.shtoppel@gmail.com>
2020-09-19 15:56:26 +0300

    fix static prefix

--- colrates/src/ecbrates/settings/common.py
+++ colrates/src/ecbrates/settings/common.py
@@ -80,4 +80,4 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/rates/static/'
